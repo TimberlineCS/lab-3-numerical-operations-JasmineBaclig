0ac800a3acd0acf828c0ad012c525b09be8e0123
JasmineBaclig <jasminebaclig@yahoo.com>
2020-09-01 18:17:14 +0000

    Finished challenges 1 and 2

--- NumericalOperations.java
+++ NumericalOperations.java
@@ -10,5 +10,20 @@ public class NumericalOperations {
     System.out.println(15 - i1 * (d1 * 3) + 4); //Problem 3: Correct
     System.out.println(15 - i1 * ((int)d1 * 3) + 4); //Problem 4: Wrong (Calculated as if cast was outside of parenthesis)
     System.out.println(15 - i1 * (int)(d1 * 3) + 4); //Problem 5: Correct
+
+    //Challenge 2
+    int num = 1234;
+
+    int ones = num % 10; //4
+    int tens = num % 100; //34
+    int hundreds = num % 1000; //234
+    int thousands = num; //1234
+
+    int firstDigit = (thousands - hundreds) / 1000; //1
+    int secondDigit = (hundreds - tens) / 100; //2
+    int thirdDigit = (tens - ones) / 10; //3
+    int fourthDigit = ones; //4
+
+    System.out.println(fourthDigit + "" + thirdDigit + "" + secondDigit + "" + firstDigit);
   }
 }
